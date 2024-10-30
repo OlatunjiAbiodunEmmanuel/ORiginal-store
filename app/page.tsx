7358c282738aa3d5708114ef72fbde3f63307a43
OlatunjiAbiodun <olatunjia889@gmail.com>
2024-10-30 17:36:30 +0100

    feat: add heroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-
+import { products } from "@/utilis/products";
+import Container from "./components/Container";
+import HeroSection from "./components/HeroSection";
+import { truncateTxt } from "@/utilis/TruncateText";
 
 export default function Home() {
   return (
-   <div>
-    Home
-   </div>  )
+    <div className="p-8">
+      <Container>
+        <div>
+          <HeroSection />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => {
+            return <div key={product.id}>{truncateTxt(product.name)}</div>;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
 }
